ab334b4fb2aa260914ed986b79673a66d4a4f60f
BossOfGames <taylorbroad@gmail.com>
2019-07-13 20:48:03 -0400

    Initial Module Creation

---
+++ Database/Seeders/MaterialCrewReduxDatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Modules\MaterialCrewRedux\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+class MaterialCrewReduxDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Model::unguard();
+
+        // $this->call("OthersTableSeeder");
+    }
+}
