9af1b94f98f4bc8674cddea0532a7a0b234b5314
Taylor Broad <taylor@cardinalhorizon.com>
2019-08-04 22:19:25 -0400

    Moved Flight Nav Drawer Card into Separate Component

---
+++ Resources/assets/js/components/SideNavFlightsCard.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="flights-card">
+        <div class="flights-title">Flights</div>
+        <div style="margin: 0 .25rem;">
+            <v-btn block color="primary"><v-icon left>add</v-icon>New Flight</v-btn>
+        </div>
+        <div class="flight-flex-container">
+            <div class="flight-button">
+                <div style="font-size: 32px; line-height: 32px;">0</div>
+                <div>Upcoming</div>
+            </div>
+            <div class="flight-button">
+                <div style="font-size: 32px; line-height: 32px;">0</div>
+                <div>Completed</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "SideNavFlightsCard"
+    }
+</script>
+
+<style scoped>
+    .flights-card {
+        background: #1a1a1a;
+        padding: .5rem;
+    }
+    .flights-title {
+        font-size: 18px;
+        text-align: center;
+        font-weight: bold;
+    }
+    .flight-flex-container {
+        display: flex;
+        margin: .5rem 0;
+        align-content: stretch;
+    }
+    .flight-button {
+        margin: 0 .25rem;
+        width: 100%;
+        padding: 1rem 0;
+        min-height: 50px;
+        background: var(--v-accent-base);
+        text-align: center;
+    }
+    .flight-button:hover {
+        transition: .2s;
+        background: var(--v-primary-base);
+        cursor: pointer;
+    }
+</style>
