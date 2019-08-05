9bd3e9dfcaedb8513faee874a4107f340ef124fc
Taylor Broad <taylor@cardinalhorizon.com>
2019-08-04 22:20:01 -0400

    Migrated to Vuetify 2.0 Stable

--- Resources/views/index.blade.php
+++ Resources/views/index.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ config('app.name') }} Crew Center</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link href="https://cdn.jsdelivr.net/npm/@mdi/font@3.x/css/materialdesignicons.min.css" rel="stylesheet">
     <link href='https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900|Material+Icons' rel="stylesheet">
     {{-- Laravel Mix - CSS File --}}
     <link rel="stylesheet" href="{{ mix('css/materialcrewredux.css') }}">
@@ -14,6 +15,14 @@
 <body>
 <div id="app"></div>
 {{-- Laravel Mix - JS File --}}
+<script>
+    window.appSettings = <?php echo json_encode([
+        'community_name' => config('app.name'),
+        'colors' => [
+            'primary' => '#71ccff'
+        ]
+    ])?>
+</script>
 <script src="{{ mix('js/materialcrewredux.js') }}"></script>
 </body>
 </html>
