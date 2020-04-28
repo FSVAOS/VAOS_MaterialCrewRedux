ed21aeef52ce39e0241474e43ac8729f9632f2b9
Taylor Broad <taylor@cardinalhorizon.com>
2020-04-28 00:05:26 -0700

    Fixed things

---
+++ Resources/assets/js/components/SimConnectionStatus.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <div class="panel-item" style="display: inline-flex; width: 100%; background: #323232">
+            <div :class="flightRules" style="border-radius: 50%; height: 25px; width: 25px; margin: auto; border: 2px solid white;">
+            </div>
+            <div style="margin-left: 1rem; flex: 1">
+                <div style="font-size: 24px; line-height: 1">{{ wx.type }} {{ wx.start_time.repr }}</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "SimConnectionStatus"
+    }
+</script>
+
+<style scoped>
+    .green {
+        background: rgba(39, 193, 84, 1);
+    }
+    .yellow {
+        background: #ded308;
+    }
+    .red {
+        background: #da1319;
+    }
+</style>
--- Resources/assets/js/views/Flights/components/FlightBriefing.vue
+++ Resources/assets/js/views/Flights/components/FlightBriefing.vue
@@ -4,10 +4,20 @@
             <v-layout row wrap fill-height>
                 <v-flex md9 xs12>
                     <div class="display-3">{{flight.callsign}}</div>
+                    <v-card>
+
+                    </v-card>
+                </v-flex>
+                <v-flex md3 xs12>
+                    <v-card>
+                        <v-card-title>Actions</v-card-title>
+                        <v-btn>Start Flight</v-btn>
+                        <v-btn>Cancel Flight</v-btn>
+                    </v-card>
                 </v-flex>
             </v-layout>
         </v-container>
-    </div>;;;;
+    </div>
 </template>
 
 <script>
